95d3cee14945f0e1e1716cfb7cf555db1fb87726
Igor Oliveira Cerqueira <igorcerqueira.ic30042008@gmail.com>
2026-04-01 22:37:09 -0300

    Funções refeitas para retornar boolean

--- atividades-express/modulos/OperadoresRelacionais.js
+++ atividades-express/modulos/OperadoresRelacionais.js
@@ -1,43 +1,73 @@
 // Seção 1 - Maior (>)
-function maiorQueDez(num1) {
-    if (num1 > 10) {
-        return "O número é maior que 10"
-    }
-}
+export function maiorQueDez(num) {
+    let resultado;
 
+    if (num > 10) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+
+    return resultado;
+}
 //Seção 2 - Menor (<)
-function menorQueCinco(num2) {
-    if (num2 < 5) {
-        return "O número é menor que 5"
-    }
-}
+export function menorQueCinco(num) {
+    let resultado;
 
+    if (num < 5) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+
+    return resultado;
+}
 // Seção 3 - Maior ou Igual (>=)
-function maiorOuIgualSete(nota) {
+export function maiorOuIgualSete(nota) {
+    let resultado;
+
     if (nota >= 7) {
-        return "Aluno aprovado"
+        resultado = true;
+    } else {
+        resultado = false;
     }
-}
 
+    return resultado;
+}
 // Seção 4 - Menor ou Igual (<=)
-function menorOuIgualVinte(temperatura) {
+export function menorOuIgualVinte(temperatura) {
+    let resultado;
+
     if (temperatura <= 20) {
-        return "Temperatura baixa"
+        resultado = true;
+    } else {
+        resultado = false;
     }
-}
 
+    return resultado;
+}
 // Seção 5 - Igual (== ou ===)
-function verificarSenha(senha) {
-    if (senha === 1234) {
-        return "Senha correta"
-    }
-}
+export function verificarSenha(senha) {
+    let resultado;
+    let senhaCorreta = 1234;
 
+    if (senha === senhaCorreta) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+
+    return resultado;
+}
 // Seção 6 - Diferente (!= ou !==)
-function verificarNumeroDiferenteZero(numero) {
-    if (numero !== 0) {
-        return "O número é diferente de zero"
-    }
-}
+export function verificarNumeroDiferenteZero(num) {
+    let resultado;
 
-export { maiorQueDez, menorQueCinco, maiorOuIgualSete, menorOuIgualVinte, verificarSenha, verificarNumeroDiferenteZero }
+    if (num !== 0) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+
+    return resultado;
+}
