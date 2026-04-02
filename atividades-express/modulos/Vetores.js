eee80df97ae7d3133c984c672952c84d04c5e5d3
Igor Oliveira Cerqueira <igorcerqueira.ic30042008@gmail.com>
2026-04-01 22:36:04 -0300

    Funções refeitas para melhorar o código

--- atividades-express/modulos/Vetores.js
+++ atividades-express/modulos/Vetores.js
@@ -1,52 +1,41 @@
 //Seção 1 VetorSomatoria (vetor de soma)
-function somarVetor(a, b, TAM) {
-    let resultado = []
-    for (let i = 0; i < TAM; i++) {
-        let soma = 0
-
-        for (let j = i; j < TAM; j++) {
-            soma += a[j]
-        }
-
-        b[i] = soma
-    }
+export function somarVetor(a, b, TAM) {
+    let resultado = [];
 
     for (let i = 0; i < TAM; i++) {
-        resultado[i] = ("B[" + i + "] = " + b[i])
+        resultado[i] = a[i] + b[i];
     }
+
     return resultado;
 }
-
 //Seção 2 VetorPontuação (vetor de remoção)
-function removerPrimeiraUltimaPontuacao(pontuacoes) {
-    let original = [...pontuacoes];
-    pontuacoes.pop();
-    pontuacoes.shift();
-    return ("Original: " + original + " | Atualizado: " + pontuacoes);
+export function removerPrimeiraUltimaPontuacao(pontuacoes) {
+    let resultado = pontuacoes;
+
+    resultado.shift();
+    resultado.pop();
+
+    return resultado;
 }
 //Seção 3 ValorProcurado (Vetor de busca)
-function encontrarValorVetor(numeros, valor) {
+export function encontrarValorVetor(numeros, valor) {
+    let resultado = false;
+
     for (let i = 0; i < numeros.length; i++) {
         if (numeros[i] === valor) {
-            return ("O valor " + valor + " foi encontrado na posição: " + i)
+            resultado = true;
         }
-        return ("O valor " + valor + " não foi encontrado na posição: " + i)
     }
-}
 
+    return resultado;
+}
 //Seção 4 lucroVenda (vetor de acumulação)
-function calcularLucroVendaPorDia(lucroDia) {
-    let lucroTotal = []
-    let soma = 0
+export function calcularLucroVendaPorDia(lucroDia) {
+    let resultado = 0;
 
-    
-
-    for (let i = 0; i < 5; i++) {
-        soma += lucroDia[i]
-        lucroTotal[i] = soma
+    for (let i = 0; i < lucroDia.length; i++) {
+        resultado += lucroDia[i];
     }
 
-    return ("Lucro por cada dia:" + lucroDia + " | Lucro total de acordo com cada dia:" + lucroTotal)
+    return resultado;
 }
-
-export { somarVetor, removerPrimeiraUltimaPontuacao, encontrarValorVetor, calcularLucroVendaPorDia }
