fc759f4cf7f21b575360957e38d7020d3aaef498
Igor Oliveira Cerqueira <igorcerqueira.ic30042008@gmail.com>
2026-04-22 19:58:57 -0300

    Convert inputs to numbers in calculation functions
    Updated functions to ensure numeric inputs are handled correctly.

--- atividades-express/modulos/DeclararVariavel.js
+++ atividades-express/modulos/DeclararVariavel.js
@@ -1,22 +1,22 @@
 //Seção 1 - CalculaMedia (let)
 function calcularMedia(nota1, nota2) {
-    let media = (nota1 + nota2) / 2;
+    let media = (Number(nota1) + Number(nota2)) / 2;
 
     return media;
 }
 
 //Seção 2 - VolumeCubo (const)
 function calcularVolumeCubo(l) {
-    const v = l * l * l
+    const v = Number(l) * Number(l) * Number(l);
 
-    return v
+    return v;
 }
 
 //Seção 3 -  NotaAprovacao (var)
 
 function aprovarNota(nota) {
 
-    if (nota >= 7) {
+    if (Number(nota) >= 7) {
         return true
     }
     return false
